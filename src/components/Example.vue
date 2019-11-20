cbdba7f87478eee1b6804db2e5a5a87423166624
Artem Filipopovskii <design.fillart@gmail.com>
2019-11-20 16:36:49 +0200

    Made a desktop version of header-top

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,18 +1,10 @@
 <template lang="pug">
   section
     .container
-      img(src="/assets/img/logo.png" :alt="message")
       p {{ message }}
 </template>
 
 <script>
-
-/*
-  !!!
-  Лучший курс по vue ;)
-  Вкусная цена по промокоду: WEBPACK
-  https://tocode.ru/curses/vuejs-s-nylya-do-pro
-*/
 
 export default {
   data () {
