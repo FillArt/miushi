4d868c17b5ecf88e2b9b439dd8d877f0d4f95736
Artem Filipopovskii <design.fillart@gmail.com>
2019-12-02 11:40:26 +0200

    Made an ipad version of the slider

--- src/js/product-slider.js
+++ src/js/product-slider.js
@@ -1,15 +1,39 @@
 window.addEventListener("load", function() {
   window._ = new Glider(document.querySelector(".glider"), {
-    slidesToShow: 4, //'auto',
-    slidesToScroll: 4,
-    itemWidth: 50,
-    draggable: true,
-    scrollLock: false,
     dots: "#dots",
-    rewind: false,
     arrows: {
       prev: ".glider-prev",
       next: ".glider-next"
     },
+    responsive: [
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 4, //'auto',
+          slidesToScroll: 4,
+          itemWidth: 150,
+          draggable: true,
+          scrollLock: false,
+          dots: "#dots",
+          rewind: false
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          itemWidth: 150
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          itemWidth: 150
+        }
+      }
+    ]
   });
 });
