2b2e494cac8ce8f092d381948842e7f48fc6c935
Artem Filipopovskii <design.fillart@gmail.com>
2019-12-02 10:32:37 +0200

    Made buttons for the desktop version of the product slider

--- src/js/product-slider.js
+++ src/js/product-slider.js
@@ -1,27 +1,4 @@
-console.log("Я подключён");
 window.addEventListener("load", function() {
-  document
-    .querySelector(".glider")
-    .addEventListener("glider-slide-visible", function(event) {
-      var glider = Glider(this);
-      console.log("Slide Visible %s", event.detail.slide);
-    });
-  document
-    .querySelector(".glider")
-    .addEventListener("glider-slide-hidden", function(event) {
-      console.log("Slide Hidden %s", event.detail.slide);
-    });
-  document
-    .querySelector(".glider")
-    .addEventListener("glider-refresh", function(event) {
-      console.log("Refresh");
-    });
-  document
-    .querySelector(".glider")
-    .addEventListener("glider-loaded", function(event) {
-      console.log("Loaded");
-    });
-
   window._ = new Glider(document.querySelector(".glider"), {
     slidesToShow: 4, //'auto',
     slidesToScroll: 4,
