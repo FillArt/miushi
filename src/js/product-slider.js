a46cf6e98589f3eca637a34eaed669c5eb0af325
Artem Filipopovskii <design.fillart@gmail.com>
2019-12-03 10:04:18 +0200

    Prepared 3 slider templates

--- src/js/product-slider.js
+++ src/js/product-slider.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function() {
-  window._ = new Glider(document.querySelector(".glider"), {
-    dots: "#dots",
+  window._ = new Glider(document.querySelector(".set"), {
+    dots: "#dots-set",
     arrows: {
       prev: ".glider-prev",
       next: ".glider-next"
@@ -14,7 +14,7 @@ window.addEventListener("load", function() {
           itemWidth: 150,
           draggable: true,
           scrollLock: false,
-          dots: "#dots",
+          dots: "#dots-set",
           rewind: false
         }
       },
@@ -26,7 +26,7 @@ window.addEventListener("load", function() {
           itemWidth: 150,
           draggable: true,
           scrollLock: false,
-          dots: "#dots",
+          dots: "#dots-set",
           rewind: false
         }
       },
@@ -38,7 +38,7 @@ window.addEventListener("load", function() {
           itemWidth: 150,
           draggable: true,
           scrollLock: false,
-          dots: "#dots",
+          dots: "#dots-set",
           rewind: false
         }
       },
@@ -56,4 +56,122 @@ window.addEventListener("load", function() {
       }
     ]
   });
+});
+window.addEventListener("load", function() {
+    window._ = new Glider(document.querySelector(".rolls"), {
+        dots: "#dots-rolls",
+        arrows: {
+            prev: ".glider-prev.rolls",
+            next: ".glider-next.rolls"
+        },
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 4, //'auto',
+                    slidesToScroll: 4,
+                    itemWidth: 150,
+                    draggable: true,
+                    scrollLock: false,
+                    dots: "#dots-rolls",
+                    rewind: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    itemWidth: 150,
+                    draggable: true,
+                    scrollLock: false,
+                    dots: "#dots-rolls",
+                    rewind: false
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    itemWidth: 150,
+                    draggable: true,
+                    scrollLock: false,
+                    dots: "#dots-rolls",
+                    rewind: false
+                }
+            },
+            {
+                breakpoint: 320,
+                settings: {
+                    slidesToShow: 1.5,
+                    slidesToScroll: 1,
+                    itemWidth: 150,
+                    draggable: true,
+                    scrollLock: false,
+                    dots: null,
+                    rewind: false
+                }
+            }
+        ]
+    });
+});
+window.addEventListener("load", function() {
+    window._ = new Glider(document.querySelector(".pizza"), {
+        dots: "#dots-pizza",
+        arrows: {
+            prev: ".glider-prev.pizza",
+            next: ".glider-next.pizza"
+        },
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 4, //'auto',
+                    slidesToScroll: 4,
+                    itemWidth: 150,
+                    draggable: true,
+                    scrollLock: false,
+                    dots: "#dots-pizza",
+                    rewind: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    itemWidth: 150,
+                    draggable: true,
+                    scrollLock: false,
+                    dots: "#dots-pizza",
+                    rewind: false
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    itemWidth: 150,
+                    draggable: true,
+                    scrollLock: false,
+                    dots: "#dots-pizza",
+                    rewind: false
+                }
+            },
+            {
+                breakpoint: 320,
+                settings: {
+                    slidesToShow: 1.5,
+                    slidesToScroll: 1,
+                    itemWidth: 150,
+                    draggable: true,
+                    scrollLock: false,
+                    dots: null,
+                    rewind: false
+                }
+            }
+        ]
+    });
 });
