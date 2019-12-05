67275ede75879fd19bfa678d557f49f3d6ee5fde
Artem Filipopovskii <design.fillart@gmail.com>
2019-12-05 13:14:36 +0200

    I started to do a block of shares

--- src/js/product-slider.js
+++ src/js/product-slider.js
@@ -234,3 +234,62 @@ window.addEventListener("load", function() {
         ]
     });
 });
+window.addEventListener("load", function() {
+    window._ = new Glider(document.querySelector(".stock"), {
+        dots: "#dots-stock",
+        arrows: {
+            prev: ".glider-prev.stock",
+            next: ".glider-next.stock"
+        },
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 4, //'auto',
+                    slidesToScroll: 4,
+                    itemWidth: 150,
+                    draggable: true,
+                    scrollLock: false,
+                    dots: "#dots-stock",
+                    rewind: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    itemWidth: 150,
+                    draggable: true,
+                    scrollLock: false,
+                    dots: "#dots-stock",
+                    rewind: false
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    itemWidth: 150,
+                    draggable: true,
+                    scrollLock: false,
+                    dots: "#dots-stock",
+                    rewind: false
+                }
+            },
+            {
+                breakpoint: 320,
+                settings: {
+                    slidesToShow: 1.5,
+                    slidesToScroll: 1,
+                    itemWidth: 150,
+                    draggable: true,
+                    scrollLock: false,
+                    dots: null,
+                    rewind: false
+                }
+            }
+        ]
+    });
+});
